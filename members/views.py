b09036baff97f73cebd7df524d6d0950d822f50e
Kelly Park <sehyun.kelly.park@gmail.com>
2022-05-04 17:26:51 -0700

    html views

--- members/views.py
+++ members/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 
 def index(request):
+    mymembers = Members.objects.all().values()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
