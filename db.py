1edd993333b3af17d0ecf9d837c6250c30d12d0a
Kelly Park <sehyun.kelly.park@gmail.com>
2022-05-09 17:11:43 -0700

    changed settings for deployment

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
+import MySQLdb
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="")
+mydb = MySQLdb.connect(host="34.102.69.250", user="root", passwd="1234")
 
 mycursor = mydb.cursor()
 
